fa28f1c3b9029159ff9fe0dfabe1e3b744125fb4
Saketh Renangi <rensaketh@gmail.com>
2023-01-28 14:14:35 -0500

    Added Google Maps Functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';
 import './App.css';
 import './LeafletMap';
 import LeafletMap from "./LeafletMap";
@@ -27,3 +27,69 @@ function App() {
 }
 
 export default App;
+*/
+
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+
+
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,  // Hides or shows the InfoWindow
+    activeMarker: {},          // Shows the active marker upon click
+    selectedPlace: {}          // Shows the InfoWindow to the selected place upon a marker
+  };
+  onMarkerClick = (props, marker, e) =>
+      this.setState({
+        selectedPlace: props,
+        activeMarker: marker,
+        showingInfoWindow: true
+      });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+  render() {
+    return (
+        <Map
+            google={this.props.google}
+            zoom={14}
+            style={mapStyles}
+            initialCenter={
+              {
+                lat: -1.2884,
+                lng: 36.8233
+              }
+            }
+        >
+          <Marker
+              onClick={this.onMarkerClick}
+              name={'Kenyatta International Convention Centre'}
+          />
+          <InfoWindow
+              marker={this.state.activeMarker}
+              visible={this.state.showingInfoWindow}
+              onClose={this.onClose}
+          >
+            <div>
+              <h4>{this.state.selectedPlace.name}</h4>
+            </div>
+          </InfoWindow>
+        </Map>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyDZWnaU7KeNuGXBswFPQQUHgwHMxR3XHfs'
+})(MapContainer);
