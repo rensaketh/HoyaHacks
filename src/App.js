9c296862078f8ba538e526ee040745c8457956a6
Saketh Renangi <rensaketh@gmail.com>
2023-01-28 18:13:22 -0500

    Added backend features

--- src/App.js
+++ src/App.js
@@ -59,35 +59,7 @@ export class MapContainer extends Component {
       });
     }
   };
-  /*render() {
-    return (
-        <Map
-            google={this.props.google}
-            zoom={14}
-            style={mapStyles}
-            initialCenter={
-              {
-                lat: -1.2884,
-                lng: 36.8233
-              }
-            }
-        >
-          <Marker
-              onClick={this.onMarkerClick}
-              name={'Kenyatta International Convention Centre'}
-          />
-          <InfoWindow
-              marker={this.state.activeMarker}
-              visible={this.state.showingInfoWindow}
-              onClose={this.onClose}
-          >
-            <div>
-              <h4>{this.state.selectedPlace.name}</h4>
-            </div>
-          </InfoWindow>
-        </Map>
-    );
-  }*/
+
     render() {
         return (
             <CurrentLocation
